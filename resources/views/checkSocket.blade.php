5e925168e338886d34539348a924f1d443ddb67d
ayra <mr.space213@mail.ru>
2023-06-24 07:22:31 +0500

    test

--- resources/views/checkSocket.blade.php
+++ resources/views/checkSocket.blade.php
@@ -1,21 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
 @vite('resources/js/app.js')
 <script>
-    setTimeout(() => {
-        window.Echo.channel('front')
-            .listen('.MyWebSocket', (e) => {
-                console.log('e', e);
-            })
-    }, 200);
+    // setTimeout(() => {
+    //     window.Echo.channel('front')
+    //         .listen('.MyWebSocket', (e) => {
+    //             console.log('e', e);
+    //         })
+    // }, 200);
+    console.log('page message');
+
+    // работает с дашбордом
+    window.Echo.channel("front").listen(".MyWebSocket", (e) => {
+        console.log('first', e);
+    });
+
+    // работает с апишкой
+    window.Echo.channel("front").listen("MyWebSocket", (e) => {
+        console.log('second', e);
+    });
 </script>
+
 </html>
