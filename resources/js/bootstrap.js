efcd2b2dd4dbff308251bec960b4db0dbce50ebe
ayra <mr.space213@mail.ru>
2023-06-24 12:46:58 +0500

    вум

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,11 +22,12 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    key: '213123123',
+    wsHost: "ws.family-clients.ru",
+    wssPort: 6002,
+    wsPort: 6002,
+    forceTLS: true,
+    disableStats: true,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    disableStatus: true,
 });
