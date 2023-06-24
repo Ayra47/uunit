7250eb4f7fb32e296e270dcf5d0511f1b6629a59
ayra <mr.space213@mail.ru>
2023-06-24 23:59:18 +0500

    ml controller

--- app/Http/Controllers/MlController.php
+++ app/Http/Controllers/MlController.php
@@ -44,15 +44,15 @@ class MlController extends Controller
                 'id' => 123,
                 'files' => [
                     [
-                        'file_name' => '12',
-                        'folder' => '12/1.pdf',
+                        'file_name' => '3.pdf',
+                        'folder' => '12',
                         'name' => 'Starts on 45',
                         'description' => 'Allowed differences:\n',
                         'page' => 1
                     ],
                     [
-                        'file_name' => '12',
-                        'folder' => '12/1.pdf',
+                        'file_name' => '1.pdf',
+                        'folder' => '12',
                         'name' => 'Starts on 34',
                         'description' => 'Allowed differences:\n',
                         'page' => 3
